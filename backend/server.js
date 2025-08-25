90b938f09ba7a9b122251541550c0b59a1f86c69
venkateshtagulla <154754274+venkateshtagulla@users.noreply.github.com>
2025-08-25 20:45:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
-//app.use(cors({ origin: 'https://your-frontend-url.onrender.com' }));
+//app.use(cors());
+app.use(cors({ origin: 'https://food-waste-management-system-67c9.onrender.com' }));
 
 app.use(express.json());
 
@@ -19,7 +19,7 @@ const dbConfig = {
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
   //ssl: { rejectUnauthorized: false },
-  ssl: process.env.DATABASE_SSL ? { rejectUnauthorized: false } : undefined,
+  ssl: process.env.DATABASE_SSL ? { rejectUnauthorized: true } : undefined,
 };
 
 // Create database connection pool
